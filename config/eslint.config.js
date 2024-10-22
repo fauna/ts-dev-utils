f711a53d03ef74677b15fa31a5f66e4596f960f0
Ashton Eby <ashton.eby@gmail.com>
2024-10-22 15:57:56 -0700

    feat: add prettier, update eslint with real rules

--- config/eslint.config.js
+++ config/eslint.config.js
@@ -1,9 +1,35 @@
-// eslint.config.js
-module.exports = [
-    {
-        rules: {
-            semi: "error",
-            "prefer-const": "error"
-        }
-    }
+// borrowed from fauna-shell
+export default [
+  {
+    ignores: ["**/node_modules", ".history"],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+
+    rules: {
+      "no-await-in-loop": "off",
+      "new-cap": "off",
+      "quote-props": "off",
+      "no-negated-condition": "off",
+      "no-warning-comments": "off",
+      "spaced-comment": "off",
+      "max-nested-callbacks": "off",
+      "no-else-return": "off",
+      "no-console": "off",
+      "no-multi-str": "off",
+      "no-prototype-builtins": "off",
+
+      "node/no-unsupported-features": "off",
+      camelcase: "off",
+    },
+  },
+  {
+    files: ["test/**/*.mjs"],
+
+    rules: {
+      "no-unused-expressions": "off",
+    },
+  },
 ];
